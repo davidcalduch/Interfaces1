5a5e0ba2936e2c35ee2af38622d29fc3b39e5d85
davidcalduch <dacalte2012@gmail.com>
2023-11-17 17:52:24 +0100

    subiendo el segundo ejercicio del dni

---
+++ DNICalcular/DNICalcular/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+namespace DNICalcular
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularLetra_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Obtener el número ingresado
+                int numero = int.Parse(txtNumero.Text);
+
+                // Calcular la letra del NIF
+                char letra = CalcularLetraNIF(numero);
+
+                // Mostrar el resultado
+                txtResultado.Text = $"La letra del NIF es: {letra}";
+            }
+            catch (FormatException)
+            {
+                // Manejar la excepción si el formato es incorrecto
+                txtResultado.Text = "Formato incorrecto. Por favor, ingrese un número válido.";
+            }
+        }
+
+        private char CalcularLetraNIF(int numero)
+        {
+            string letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+            int indice = numero % 23;
+            return letras[indice];
+        }
+    }
+}
